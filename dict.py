40563fc67351913592ced1a9394fc1ef7db2cbb2
bengazy <bankomah63@gmail.com>
2025-11-30 18:03:50 +0000

    new files

---
+++ dict.py
@@ -0,0 +1,12 @@
+import math 
+'''
+dict = {'name':'benjamin', 'age':45, 'height':7.1}
+l = [1,2,3,4,5,1,1]
+t = 10,'dog',30,'goat'
+a=l.count(1)
+#print(a)
+#for i in dict:
+if 'age' in dict:
+    s = dict.get('dav','not available')
+    print(f"name {s}")'''
+print(math.sqrt(25))
